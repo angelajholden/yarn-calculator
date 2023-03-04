3383d2f111f777a8476b81709f5032c041de13f6
Angela Holden <angelajholden@gmail.com>
2023-03-04 10:15:38 -0600

    removing unused files

--- dist/js/scripts.js
+++
@@ -1,9 +0,0 @@
-function copyrightYear() {
-	const copyright = document.getElementById("copy_date");
-	const year = new Date();
-	copyright.innerHTML = year.getFullYear();
-}
-
-window.onload = () => {
-	copyrightYear();
-};
